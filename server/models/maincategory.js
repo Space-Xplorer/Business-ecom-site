0eb0f78817e42f96582a104286fc369116c45246
Mahesh Patnala <maheshwarpatn@gmail.com>
2025-06-25 18:49:54 +0530

    carousel

--- server/models/maincategory.js
+++
@@ -1,9 +0,0 @@
-// models/MainCategory.js
-const mongoose = require('mongoose');
-
-const mainCategorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  apparelTypes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ApparelType' }]
-});
-
-module.exports = mongoose.model('MainCategory', mainCategorySchema);
