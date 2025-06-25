4959257ffdc6072588ed4a0a2878b217c73b63c9
SpoorthyBoga <spoorthymaharaju@gmail.com>
2025-06-25 18:50:39 +0530

    change in routes

--- server/models/appareltype.js
+++ server/models/appareltype.js
@@ -2,9 +2,14 @@
 const mongoose = require('mongoose');
 
 const apparelTypeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  mainCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'MainCategory', required: true },
+  name: String,
+  mainCategory: {
+    type: String, // <--- Change from ObjectId to String
+    enum: ['Men', 'Women', 'Kids', 'All', 'General'], // Optional: enforce allowed values
+    required: true
+  },
   subcategories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory' }]
 });
+
 
 module.exports = mongoose.model('ApparelType', apparelTypeSchema);
